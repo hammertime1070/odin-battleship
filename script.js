3469b22cab281eaae5484e7e6e1d31cf726d09d1
hammertime1070 <mbkeith614@gmail.com>
2023-10-27 11:51:56 -0400

    Add placeShipVertically method

--- script.js
+++ script.js
@@ -18,14 +18,14 @@ class Ship {
 
 class Gameboard {
     constructor() {
-        this.board = createBoard()
+        this.board = this.createBoard()
     }
 
     createBoard() {
-        newBoard = []
-        for (let i=0; i < 9; i++) {
+        let newBoard = []
+        for (let i=0; i < 10; i++) {
             let row=[]
-            for (let j=0; j < 9; j++) {
+            for (let j=0; j < 10; j++) {
                 let square = new Square(i, j)
                 row.push(square)
             }
@@ -33,13 +33,40 @@ class Gameboard {
         }
         return newBoard
     }
+
+    getSquare(x, y) {
+        return this.board[x][y]
+    }
+
+    placeShipVertically(x, y, length) {
+        // Get Squares for each place a ship should be placed
+        let shipPositions = []
+        for (let i=0; i < length; i++) {
+            let square = this.getSquare(x,y-i)
+            shipPositions.push(square)
+        }
+        // Make sure all of the Ship Positions are valid
+        if (!shipPositions.every(element => element != null && !element.occupied)) {
+            console.log("There is not enough space to place a ship here")
+            return false
+        }
+        // create the new Ship object
+        const ship = new Ship(length)
+        // set the occupant of each square to the ship and make it as occupied
+        for (square of shipPositions) {
+            square.occupant = ship
+            square.occupied = true
+        }
+    }
 }
 
 class Square {
     constructor(x, y) {
         this.x = x
         this.y = y
+        this.occupied = false
+        this.occupant = null
     }
 }
 
-module.exports = { Ship, Gameboard };
+module.exports = { Ship, Gameboard, Square };
--- script.test.js
+++ script.test.js
@@ -1,7 +1,101 @@
-const { Ship } = require('./script')
+const { Ship, Gameboard, Square } = require('./script')
 
 test('Ship hit function', () => {
     const ship = new Ship(2)
     ship.hit()
     expect(ship.timesHit).toBe(1)
 })
+
+describe('Gameboard', () => {
+    test('should create a 10x10 board', () => {
+        const gameboard = new Gameboard()
+        const board = gameboard.board
+
+        expect(board.length).toBe(10)
+        for (const row of board) {
+            expect(row.length).toBe(10)
+        }
+    })
+
+    test('should populate the board with Square objects', () => {
+        const gameboard = new Gameboard()
+        const board = gameboard.board
+
+        for (const row of board) {
+            for (const square of row) {
+                expect(square).toBeInstanceOf(Square)
+            }
+        }
+    })
+    test('should initialize each square with the correct coordinates', () => {
+        const gameboard = new Gameboard()
+        const board = gameboard.board
+
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                expect(board[i][j].x).toBe(i)
+                expect(board[i][j].y).toBe(j)
+            }
+        }
+    })
+})
+
+describe('Gameboard', () => {
+    let gameboard;
+
+    beforeEach(() => {
+        gameboard = new Gameboard();
+    });
+
+    test('should place a ship vertically successfully', () => {
+        const x = 2;
+        const y = 5;
+        const length = 3;
+        gameboard.placeShipVertically(x, y, length);
+
+        for (let i = 0; i < length; i++) {
+            const square = gameboard.getSquare(x, y - i);
+            expect(square.occupant).toBeInstanceOf(Ship);
+            expect(square.occupied).toBe(true);
+        }
+    });
+
+    test('should not place a ship if not enough space vertically', () => {
+        const x = 2;
+        const y = 1;
+        const length = 4;
+        const result = gameboard.placeShipVertically(x, y, length);
+
+        expect(result).toBe(null);
+        for (let i = 0; i < gameboard.board.length; i++) {
+            for (let j = 0; j < gameboard.board[i].length; j++) {
+                expect(gameboard.board[i][j].occupant).toBe(null);
+                expect(gameboard.board[i][j].occupied).toBe(false);
+            }
+        }
+    });
+
+    test('should not place a ship if starting coordinates are out of bounds', () => {
+        const x = -1;
+        const y = 5;
+        const length = 3;
+        const result = gameboard.placeShipVertically(x, y, length);
+
+        expect(result).toBe(null);
+        for (let i = 0; i < gameboard.board.length; i++) {
+            for (let j = 0; j < gameboard.board[i].length; j++) {
+                expect(gameboard.board[i][j].occupant).toBe(null);
+                expect(gameboard.board[i][j].occupied).toBe(false);
+            }
+        }
+    });
+
+    // Add any other test cases you think are necessary
+});
+
+
+
+
+
+
+
