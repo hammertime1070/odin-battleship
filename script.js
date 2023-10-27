2017f7a9401e8daad1320d005442ba0a6d43b904
hammertime1070 <mbkeith614@gmail.com>
2023-10-27 10:44:43 -0400

    Add Gameboard class

--- script.js
+++ script.js
@@ -18,7 +18,27 @@ class Ship {
 
 class Gameboard {
     constructor() {
+        this.board = createBoard()
+    }
 
+    createBoard() {
+        newBoard = []
+        for (let i=0; i < 9; i++) {
+            let row=[]
+            for (let j=0; j < 9; j++) {
+                let square = new Square(i, j)
+                row.push(square)
+            }
+            newBoard.push(row)
+        }
+        return newBoard
+    }
+}
+
+class Square {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
     }
 }
 
